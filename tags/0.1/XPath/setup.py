450d263d2ba9ec2c08deeaa559bb78938c0f9add
damien.neil <damien.neil@8210bc9a-ff2d-11dd-984d-ab748d24aa7e>
2009-02-22 05:29:18 +0000

    fix setup.py version

--- tags/0.1/XPath/setup.py
+++ tags/0.1/XPath/setup.py
@@ -49,12 +49,20 @@ class build_py(_build_py):
 
         return modules
 
-setup(name="xpath",
-      version="1.0",
+setup(name="py-dom-xpath",
+      version="0.1",
       description="XPath for DOM trees",
+      long_description="""\
+py-dom-xpath is a pure Python implementation of XPath 1.0. It
+supports almost all XPath 1.0, with the main exception being the
+namespace axis. It operates on DOM 2.0 nodes, and works well with
+xml.dom.minidom.
+
+py-dom-xpath requires Python 2.5 or greater.""",
       author='Damien Neil',
       author_email='damien.neil@gmail.com',
       url='http://code.google.com/p/py-dom-xpath/',
+      download_url='http://py-dom-xpath.googlecode.com/files/py-dom-xpath-0.1.tar.gz',
       packages=['xpath'],
       cmdclass={
           'build_py':build_py,
